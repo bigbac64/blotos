999187fd8dd1c945e28dbafac5d2129df26a3bb8
adesobry <antoine@kwyk.fr>
2026-02-04 19:19:31 +0100

    writer fonctionnel

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,20 +1,14 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
 mod framebuffer_adapter;
-mod console;
 mod writer;
 
 use framebuffer_adapter::FramebufferAdapter;
-use embedded_graphics::{
-    pixelcolor::Rgb888,
-    prelude::*,
-    text::Text,
-};
 use bootloader_api::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use embedded_graphics::mono_font::ascii::FONT_9X18;
-use embedded_graphics::mono_font::MonoTextStyleBuilder;
+use writer::Terminal;
 
 entry_point!(kernel_main);
 
@@ -31,18 +25,8 @@ fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
         info
     );
 
-    // Create a new character style
-    let style = MonoTextStyleBuilder::new()
-        .font(&FONT_9X18)
-        .text_color(Rgb888::new(0, 255, 0))  // Vert phosphore
-        .background_color(Rgb888::BLACK)
-        .build();
-
-    // Create a text at position (20, 30) and draw it using the previously defined style
-    Text::new("Hello ma meuringue !", Point::new(20, 30), style)
-        .draw(&mut display).expect("Drawing failed");
-    Text::new("T'es la plus belle", Point::new(20, 50), style)
-        .draw(&mut display).expect("Drawing failed");
+    let mut terminal = Terminal::new(&mut display);
+    let _ = writeln!(&mut terminal, "hello !\nc'est pas possible");
 
     loop {}
 }
--- kernel/src/writer.rs
+++ kernel/src/writer.rs
@@ -1,14 +1,67 @@
+use core::fmt::Write;
+use embedded_graphics::mono_font::ascii::FONT_9X18;
+use embedded_graphics::mono_font::{MonoTextStyle, MonoTextStyleBuilder};
+use embedded_graphics::{
+    pixelcolor::Rgb888,
+    prelude::*,
+    text::Text,
+};
 use embedded_graphics::prelude::Point;
+use crate::framebuffer_adapter::FramebufferAdapter;
 
-struct BufferWrite{
-    position: Point,
-    offset: Point,
-    padding_line: Point,
+pub(crate) struct Terminal<'a>{
+    display: &'a mut FramebufferAdapter<'a>,
+    position: Point, // position des caractère col, line
+    // cursor: Point, // position du curseur
+    offset: Point, // espace pris par caractère en pixel
+    padding_line: Point, // espace x entre le bord de l'écran, espace y entre chaque ligne
+    style: MonoTextStyle<'a, Rgb888>,
 }
 
+impl<'a> Terminal<'a> {
+    pub fn new(display: &'a mut FramebufferAdapter<'a>) -> Self {
+        let style = MonoTextStyleBuilder::new()
+            .font(&FONT_9X18)
+            .text_color(Rgb888::new(0, 255, 0))  // Vert phosphore
+            .background_color(Rgb888::BLACK)
+            .build();
+        Self {
+            display,
+            position: Point::new(0, 1),
+            // cursor: Point::zero(),
+            offset: Point::new(9, 18),
+            padding_line: Point::new(5, 5),
+            style,
+        }
+    }
 
-impl BufferWrite {
-    pub fn new(offset: Point) -> Self {
-        Self { position: Point::zero(), offset, padding_line: Point::zero() }
+    pub fn pixel_align(&self) -> Point{
+        Point::new(self.position.x * (self.offset.x + self.padding_line.x), self.position.y * (self.offset.y + self.padding_line.y))
+    }
+
+    pub fn new_line(&mut self) {
+        self.position.y += 1;
+        self.position.x = 0;
+    }
+}
+
+impl<'a> Write for Terminal<'a> {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        let collection = s.split('\n');
+        let count = collection.clone().count();
+        for (i, line) in collection.into_iter().enumerate()  {
+            Text::new(line, self.pixel_align(), self.style).draw(self.display).expect("Write error");
+            self.position.x += line.len() as i32;
+            if i != count - 1 {
+                self.new_line();
+            }
+        }
+
+        Ok(())
+    }
+
+    fn write_char(&mut self, c: char) -> core::fmt::Result {
+        let mut buf = [0u8; 4]; // tableau d'octet pour utf-8 (max 4 bytes)
+        self.write_str(c.encode_utf8(&mut buf))
     }
 }
